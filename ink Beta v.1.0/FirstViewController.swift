9800687364d5e531e8355997b927b5717abe6d02
Anthony <alascala@argen>
2017-02-26 18:56:22 -0800

    Initial Commit

---
+++ ink Beta v.1.0/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ink Beta v.1.0
+//
+//  Created by Anthony La Scala on 2/26/17.
+//  Copyright © 2017 TuBlink. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ink Beta v.1.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ink Beta v.1.0
+//
+//  Created by Anthony La Scala on 2/26/17.
+//  Copyright © 2017 TuBlink. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
